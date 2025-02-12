a9550a53c36645ac754033a7104b883ddf3c1e79
Kai Pereira <88850028+KaiPereira@users.noreply.github.com>
2025-02-12 20:46:30 +0000

    fixing some bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${vt323.variable}`}>
         {children}
       </body>
     </html>
